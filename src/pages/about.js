212095f4f19234394c8db520cca3d313b2a14c9a
koashima <48111257+koashima@users.noreply.github.com>
2020-06-11 14:51:08 -0700

    Add Header component and About page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function About() {
+  return ( 
+    <div style={{ color: `teal` }}>
+      <Header />
+      <p>I am a web developer primarily</p>
+    </div>
+  )
+}
