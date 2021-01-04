bb8eae764cb02aedb97afbd6f1af216c224d7e50
koashima <48111257+koashima@users.noreply.github.com>
2021-01-03 16:49:44 -1000

    prettify contact page

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,17 @@
 
 module.exports = {
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-dark-mode`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "project",
+        path: "./data",
+      },
+    },
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,34 @@
-import React from 'react'
-import Header from '../components/Header'
-import Layout from '../components/Layout';
+import React from "react"
+import Header from "../components/Header"
+import Layout from "../components/Layout"
 
-function Contact() { 
+function Contact() {
   return (
     <Layout>
       <Header headerText="CONTACT" />
       <h2>Find me on any of these platforms!</h2>
       <ul>
-        <li> <a href="https://twitter.com/koashima" target="_blank">twitter</a></li>
-        <li> <a href="https://linkedin.com/in/davidshimabukuro" target="_blank">linkedin</a></li>
-        <li> <a href="https://github.com/koashima " target="_blank">github</a></li>
+        <li>
+          {" "}
+          <a href="https://twitter.com/koashima" target="_blank" rel="noreferrer">
+            twitter
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://linkedin.com/in/davidshimabukuro" target="_blank" rel="noreferrer">
+            linkedin
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://github.com/koashima " target="_blank" rel="noreferrer">
+            github
+          </a>
+        </li>
       </ul>
     </Layout>
   )
 }
 
-export default Contact;
+export default Contact
