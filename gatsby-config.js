bcc75daa58c4ff01fe5bf360f27a78d43b17a009
koashima <48111257+koashima@users.noreply.github.com>
2021-03-03 14:30:07 -1000

    Add AboutPage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "portfolio",
+    title: "koa",
   },
   plugins: [
     `gatsby-plugin-gatsby-cloud`,
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutPage = () => {
+  
+  return(
+    <main>
+      <title>about koa</title>
+      <h2>about koa</h2>
+      <p>koa enjoys creating things, having conversations with people, and growing in all ways.</p>
+    </main>
+  )
+}
+
+export default AboutPage
