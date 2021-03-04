ef17750943678455fed9a0457f3b084398b528d9
koashima <48111257+koashima@users.noreply.github.com>
2021-03-04 13:37:10 -1000

    Add simple content for about and contact.. since the site is live right now

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,10 +5,18 @@ const AboutPage = () => {
   return (
     <Layout pageTitle="about koa">
       <main>
-        <h2>about koa</h2>
+        <h2>
+          <span style={{ color: '#eb5e28' }}>about</span> koa
+        </h2>
         <p>
           koa enjoys creating things, having conversations with people, and
           growing in all ways.
+        </p>
+        <p>
+          if you're reading this,
+          <span style={{ color: '#eb5e28' }}> lucky you</span> . if you check
+          back i'm sure you will see much improvement since i am developing this
+          site as you read this.
         </p>
       </main>
     </Layout>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,8 +4,21 @@ import Layout from '../components/layout';
 const Contact = () => {
   return (
     <Layout pageTitle="contact koa">
-      <h2>contact koa</h2>
+      <h2>
+        <span style={{ color: '#eb5e28' }}>contact</span> koa
+      </h2>
       <p>contact koa via these methods</p>
+      <ul>
+        <li>
+          <a href="https://github.com/koashima">github</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/koashima">twitter</a>
+        </li>
+        <li>
+          <a href="https://instagram.com/koashima">instagram</a>
+        </li>
+      </ul>
     </Layout>
   );
 };
