4c861c833dcae9f5a15d7c422be22b69e2374afc
koashima <48111257+koashima@users.noreply.github.com>
2021-03-05 14:21:55 -1000

    Fix gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,26 +9,24 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: `gatsby-remark-images`,
       options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 590,
-            },
-          },
-        ],
+        maxWidth: 590,
       },
     },
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/content/projects/`,
       },
     },
     {
