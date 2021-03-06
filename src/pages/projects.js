2235c336db357ab3ab736481a5dc979922c8d721
koashima <48111257+koashima@users.noreply.github.com>
2021-03-05 17:08:54 -1000

    Add page query for all project posts filter by ext

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-const Projects = () => {
+const Projects = ({ data }) => {
   return (
     <Layout pageTitle="projects">
       <h2>projects of koa</h2>
       <p>check out all my projects</p>
+      <ul>
+        {data.allFile.nodes.map((node) => {
+          return <li key={node.name}>{node.name}</li>;
+        })}
+      </ul>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query ProjPosts {
+    allFile(
+      filter: { dir: { regex: "/content/projects/" }, ext: { regex: "/.mdx/" } }
+    ) {
+      nodes {
+        extension
+        name
+      }
+    }
+  }
+`;
 
 export default Projects;
