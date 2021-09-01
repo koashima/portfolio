b1eae38df0047ed3d05462f1e5633624549fe440
koashima <48111257+koashima@users.noreply.github.com>
2021-09-01 10:24:04 -1000

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,21 +3,32 @@ import Layout from '../components/layout';
 
 const AboutPage = () => {
   return (
-    <Layout >
+    <Layout>
       <main>
         <h1>
-          <span style={{ color: '#eb5e28' }}>about.</span> 
+          <span style={{ color: '#eb5e28' }}>about.</span>
         </h1>
-        <p>
-          Koa enjoys creating things, having conversations with people, and
-          growing in all ways.
-        </p>
-        <p>
-          If you're reading this,
-          <span style={{ color: '#eb5e28' }}> lucky you</span>. If you check
-          back I'm sure you will see much improvement to the site, since I am developing this
-          site as you read this.
-        </p>
+        <h2>
+          Hello, my name is Koa. 
+          <br />
+          <br /> 
+          I am a freelance web developer, who also offers
+          digital support services.
+          <br />
+          <br />
+          Please fill out the form on the homepage to reach out.
+          <br/>
+          <br/>
+          You can do this by clicking or pressing the <span style={{ color: '#eb5e28' }}> koa</span> on the top left corner of the screen.
+          <br/>
+          <br/>
+          Alternatively, you can also check out the <span style={{ color: '#eb5e28' }}>contact</span> page for links to my online social presence.
+          <br/>
+          <br/>
+          Don't miss checking out some of the <span style={{ color: '#eb5e28' }}>projects</span> I've created. 
+          <br/>
+          Please note that this is not a comprehensive list.
+        </h2>
       </main>
     </Layout>
   );
